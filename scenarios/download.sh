1db6ca843ad345c383d8fcd2a16f362b82e9a415
Blottiere Paul <blottiere.paul@gmail.com>
2018-06-13 00:30:00 +0100

    Update scenarios

---
+++ scenarios/download.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+URL=http://37.187.164.233/qgis-server-perfsuite/
+
+mkdir -p data
+
+wget $URL/eu_dem_v11.tif -P ./data
+wget $URL/eu_dem_v11.tif.ovr -P ./data
+wget $URL/opendata_disclaimer.md -P ./data
+wget $URL/data_perf.qgs -P ./data
+wget $URL/data_perf_view.qgs -P ./data
+wget $URL/data_perf_view_trust.qgs -P ./data
+wget $URL/data_perf_parallel_labelling.qgs -P ./data
+wget $URL/data_perf_parallel_labelling_8_candidates.qgs -P ./data
