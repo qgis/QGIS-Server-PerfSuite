453c7644876fe8f398fcce8463208f993ffeb765
Blottiere Paul <blottiere.paul@gmail.com>
2018-06-14 14:17:27 +0100

    Update tif dem

--- scenarios/download.sh
+++ scenarios/download.sh
@@ -4,8 +4,8 @@ URL=http://37.187.164.233/qgis-server-perfsuite/
 
 mkdir -p data
 
-wget $URL/eu_dem_v11.tif -P ./data
-wget $URL/eu_dem_v11.tif.ovr -P ./data
+wget $URL/eu_dem_v11_hillshade.tif -P ./data
+wget $URL/eu_dem_v11_hillshade.tif.ovr -P ./data
 wget $URL/opendata_disclaimer.md -P ./data
 wget $URL/data_perf.qgs -P ./data
 wget $URL/data_perf_view.qgs -P ./data
