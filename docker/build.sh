a4d8c62ecec83338b5ed7958378134858d7186fa
Blottiere Paul <blottiere.paul@gmail.com>
2018-06-19 13:27:59 +0100

    Build nginx container on the fly and update scenario configuration file

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,9 @@
 #! /usr/bin/env bash
 
-BASEDIR=$(dirname "$0")
+BASEDIR=$(dirname $(realpath -s $0))
 
 cd ${BASEDIR}/2.14 && sh build.sh
 cd ${BASEDIR}/2.18 && sh build.sh
 cd ${BASEDIR}/3.0 && sh build.sh
 cd ${BASEDIR}/master && sh build.sh
 cd ${BASEDIR}/data && sh build.sh
-cd ${BASEDIR}/nginx && sh build.sh
--- scenarios/run.sh
+++ scenarios/run.sh
@@ -22,12 +22,6 @@ cd $ROOT
 docker-compose up -d
 
 # get ip for containers
-DOCKER_IP_2_14=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' qgisserver-perfsuite-2.14)
-DOCKER_IP_2_18=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' qgisserver-perfsuite-2.18)
-DOCKER_IP_3_0=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' qgisserver-perfsuite-3.0)
-DOCKER_IP_3_0_PARALLEL_RENDERING=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' qgisserver-perfsuite-3.0-parallel-rendering)
-DOCKER_IP_MASTER=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' qgisserver-perfsuite-master)
-DOCKER_IP_MASTER_PARALLEL_RENDERING=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' qgisserver-perfsuite-master-parallel-rendering)
 DOCKER_IP_DATA=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' qgisserver-perfsuite-data)
 
 # wait for postgres to be ready
@@ -44,15 +38,6 @@ docker exec -it qgisserver-perfsuite-3.0 ls /data > /dev/null
 docker exec -it qgisserver-perfsuite-3.0-parallel-rendering ls /data > /dev/null
 docker exec -it qgisserver-perfsuite-master ls /data > /dev/null
 docker exec -it qgisserver-perfsuite-master-parallel-rendering ls /data > /dev/null
-
-# prepare scenario configuration file with ips of containers
-cp scenarios.sample.yml scenarios.yml
-sed -i "s/{{ URL_2_14 }}/$DOCKER_IP_2_14/g" scenarios.yml
-sed -i "s/{{ URL_2_18 }}/$DOCKER_IP_2_18/g" scenarios.yml
-sed -i "s/{{ URL_3_0 }}/$DOCKER_IP_3_0/g" scenarios.yml
-sed -i "s/{{ URL_3_0_PARALLEL_RENDERING }}/$DOCKER_IP_3_0_PARALLEL_RENDERING/g" scenarios.yml
-sed -i "s/{{ URL_MASTER }}/$DOCKER_IP_MASTER/g" scenarios.yml
-sed -i "s/{{ URL_MASTER_PARALLEL_RENDERING }}/$DOCKER_IP_MASTER_PARALLEL_RENDERING/g" scenarios.yml
 
 # run graffiti
 if [ ! -d "$ROOT/graffiti" ]
