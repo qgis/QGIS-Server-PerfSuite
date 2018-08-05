0a01dd94a0b2d747702ecb51c76e1eeb21f60b37
Blottiere Paul <blottiere.paul@gmail.com>
2018-08-05 15:14:06 +0200

    Update ci script

--- ansible/roles/ci/files/ci.sh
+++ ansible/roles/ci/files/ci.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+GRAFFITI=/tmp/graffiti
+DATE=$(date +"%Y_%m_%d_%H_%M")
 
 echo "Remove docker image for master"
 echo "------------------------------"
@@ -11,3 +13,9 @@ cd docker/master && sh build.sh && cd -
 echo "Run graffiti"
 echo "------------"
 cd scenarios && sh run.sh && cd -
+rm -rf $GRAFFITI
+cd scenarios && sh run.sh && cd -
+if [ -d $GRAFFITI ]
+then
+  scp -r $GRAFFITI qgis-test:/var/www/qgisdata/QGIS-tests/perf_test/graffiti/$DATE
+fi
