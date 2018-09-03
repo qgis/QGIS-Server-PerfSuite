c7c7268e40395c56c5e360c1f11df574fff63e01
Blottiere Paul <blottiere.paul@gmail.com>
2018-09-03 19:49:44 +0100

    Update scenarios

--- scenarios/download.sh
+++ scenarios/download.sh
@@ -7,7 +7,8 @@ mkdir -p data
 wget $URL/eu_dem_v11_hillshade.tif -P ./data
 wget $URL/eu_dem_v11_hillshade.tif.ovr -P ./data
 wget $URL/opendata_disclaimer.md -P ./data
-wget $URL/data_perf.qgs -P ./data
+wget $URL/data_perf_qgis2.qgs -P ./data
+wget $URL/data_perf_qgis3.qgs -P ./data
 wget $URL/data_perf_view.qgs -P ./data
 wget $URL/data_perf_view_trust.qgs -P ./data
 wget $URL/data_perf_parallel_labelling.qgs -P ./data
