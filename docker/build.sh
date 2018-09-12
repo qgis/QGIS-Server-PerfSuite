5482de416a2f4e41832df6dbead786e93039702a
Blottiere Paul <blottiere.paul@gmail.com>
2018-09-12 20:25:50 +0200

    Add Dockerfile for QGIS 3.2

--- docker/build.sh
+++ docker/build.sh
@@ -5,5 +5,6 @@ BASEDIR=$(dirname $(realpath -s $0))
 cd ${BASEDIR}/2.14 && sh build.sh
 cd ${BASEDIR}/2.18 && sh build.sh
 cd ${BASEDIR}/3.0 && sh build.sh
+cd ${BASEDIR}/3.2 && sh build.sh
 cd ${BASEDIR}/master && sh build.sh
 cd ${BASEDIR}/data && sh build.sh
