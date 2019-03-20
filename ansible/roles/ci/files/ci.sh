883e5c4ff807f90c921303874a1bc83b1c3049c4
Blottiere Paul <blottiere.paul@gmail.com>
2019-03-20 16:04:55 +0100

    Update ci script

--- ansible/roles/ci/files/ci.sh
+++ ansible/roles/ci/files/ci.sh
@@ -5,15 +5,15 @@ DATE=$(date +"%Y_%m_%d_%H_%M")
 echo "Remove docker images"
 echo "--------------------"
 docker rmi qgisserver-perfsuite/2.18
-docker rmi qgisserver-perfsuite/3.2
 docker rmi qgisserver-perfsuite/3.4
+docker rmi qgisserver-perfsuite/3.6
 docker rmi qgisserver-perfsuite/master
 
 echo "Build new docker images"
 echo "-----------------------"
 cd docker/2.18 && sh build.sh && cd -
-cd docker/3.2 && sh build.sh && cd -
 cd docker/3.4 && sh build.sh && cd -
+cd docker/3.6 && sh build.sh && cd -
 cd docker/master && sh build.sh && cd -
 
 echo "Run graffiti"
