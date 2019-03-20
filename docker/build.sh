14c2823a8e153903efb7dd17835499bc82415b93
Blottiere Paul <blottiere.paul@gmail.com>
2019-03-20 10:39:37 +0100

    Add docker scripts for QGIS 3.6

--- docker/build.sh
+++ docker/build.sh
@@ -6,5 +6,6 @@ cd ${BASEDIR}/2.14 && sh build.sh
 cd ${BASEDIR}/2.18 && sh build.sh
 cd ${BASEDIR}/3.2 && sh build.sh
 cd ${BASEDIR}/3.4 && sh build.sh
+cd ${BASEDIR}/3.6 && sh build.sh
 cd ${BASEDIR}/master && sh build.sh
 cd ${BASEDIR}/data && sh build.sh
